e0501452e81593306435c24a26e855669a2ceea8
Betrezen88 <michal.s.wyszynski@gmail.com>
2020-03-23 10:52:55 +0100

    Added core subproject.

---
+++ core/Card.cpp
@@ -0,0 +1,5 @@
+#include "Card.h"
+
+Core::Core()
+{
+}
---
+++ core/Card.h
@@ -0,0 +1,12 @@
+#ifndef CORE_H
+#define CORE_H
+
+#include "core_global.h"
+
+class CORE_EXPORT Core
+{
+public:
+    Core();
+};
+
+#endif // CORE_H
