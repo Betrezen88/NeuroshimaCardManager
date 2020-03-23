6c6fefb427dcc36a07c2aa6a2e773aebf9e8f5a2
Betrezen88 <michal.s.wyszynski@gmail.com>
2020-03-23 19:12:40 +0100

    Added stats object to Card class.

--- core/Card/Card.cpp
+++ core/Card/Card.cpp
@@ -4,3 +4,13 @@ Card::Card(QObject *parent) : QObject(parent)
 {
 
 }
+
+Stats *Card::stats() const
+{
+    return m_stats;
+}
+
+void Card::setStats(Stats *stats)
+{
+    m_stats = stats;
+}
--- core/Card/Card.h
+++ core/Card/Card.h
@@ -5,10 +5,20 @@
 
 #include <QObject>
 
+class Stats;
+
 class CORE_EXPORT Card : public QObject
 {
 public:
     explicit Card(QObject *parent = nullptr);
+
+    Stats *stats() const;
+
+public slots:
+    void setStats(Stats *stats);
+
+private:
+    Stats *m_stats{nullptr};
 };
 
 #endif // CORE_H
