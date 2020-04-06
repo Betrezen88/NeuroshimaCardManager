ade45ef05b3a2e74e64c989457aa9724bae1e0ad
Betrezen88 <michal.s.wyszynski@gmail.com>
2020-04-06 21:07:38 +0200

    Registered cpp wrappers to qml.

--- CardManagerQml/main.cpp
+++ CardManagerQml/main.cpp
@@ -1,13 +1,40 @@
-#include <QGuiApplication>
+﻿#include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "Utils/DataReader.h"
+
+#include "cpp/Card/CardWrapper.h"
+#include "cpp/Page/StatsWrapper.h"
+#include "cpp/Section/PersonalWrapper.h"
+#include "cpp/Element/DiseaseWrapper.h"
+#include "cpp/Element/OriginWrapper.h"
+#include "cpp/Element/ProfessionWrapper.h"
+#include "cpp/Element/SpecializationWrapper.h"
+#include "cpp/Element/FeatureWrapper.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
+    qmlRegisterType<CardWrapper>("cpp", 1, 0, "CardWrapper");
+    qmlRegisterType<StatsWrapper>("cpp", 1, 0, "StatsWrapper");
+    qmlRegisterType<PersonalWrapper>("cpp", 1, 0, "PersonalWrapper");
+    qmlRegisterType<DiseaseWrapper>("cpp", 1, 0, "DiseaseWrapper");
+    qmlRegisterType<OriginWrapper>("cpp", 1, 0, "OriginWrapper");
+    qmlRegisterType<ProfessionWrapper>("cpp", 1, 0, "ProfessionWrapper");
+    qmlRegisterType<SpecializationWrapper>("cpp", 1, 0, "SpecializationWrapper");
+    qmlRegisterType<FeatureWrapper>("cpp", 1, 0, "FeatureWrapper");
+
     QGuiApplication app(argc, argv);
 
+    DataReader reader;
+    CardWrapper *pCard = new CardWrapper(reader.loadCard("c:/Users/Michał/Documents/!NsHeroes/Nick 'Happy' Sax.json"), &app);
+
     QQmlApplicationEngine engine;
+
+    engine.rootContext()->setContextProperty("pCardData", pCard);
+
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
