0cd666f755cb0109dfaeab924bec5c6eb6face3f
Betrezen88 <michal.s.wyszynski@gmail.com>
2020-04-15 14:36:30 +0200

    Added logic to handle attributes by wrapper.

--- CardManagerQml/cpp/Page/StatsWrapper.cpp
+++ CardManagerQml/cpp/Page/StatsWrapper.cpp
@@ -1,6 +1,8 @@
 ﻿#include "StatsWrapper.h"
 #include "cpp/Section/PersonalWrapper.h"
+#include "cpp/Element/AttributeWrapper.h"
 #include "Card/Pages/StatsData.h"
+#include "Card/Elements/Stats/AttributeData.h"
 
 StatsWrapper::StatsWrapper(QObject *parent) : QObject(parent)
 {
@@ -12,10 +14,61 @@ StatsWrapper::StatsWrapper(StatsData *statsData, QObject *parent)
       m_pStatsData(statsData),
       m_pPersonalWrapper(new PersonalWrapper(statsData->personal(), this))
 {
-
+    for ( AttributeData *pAttributeData: m_pStatsData->attributes() ) {
+        appendAttribute( new AttributeWrapper(pAttributeData, this) );
+    }
 }
 
 PersonalWrapper *StatsWrapper::personal() const
 {
     return m_pPersonalWrapper;
+}
+
+QQmlListProperty<AttributeWrapper> StatsWrapper::attributes()
+{
+    return QQmlListProperty<AttributeWrapper>(this, this,
+                                              &StatsWrapper::appendAttribute,
+                                              &StatsWrapper::attributeCount,
+                                              &StatsWrapper::attribute,
+                                              &StatsWrapper::clearAttributes);
+}
+
+void StatsWrapper::appendAttribute(AttributeWrapper *a)
+{
+    m_attributes.push_back(a);
+}
+
+int StatsWrapper::attributeCount() const
+{
+    return m_attributes.count();
+}
+
+AttributeWrapper *StatsWrapper::attribute(int index) const
+{
+    return m_attributes.at(index);
+}
+
+void StatsWrapper::clearAttributes()
+{
+    m_attributes.clear();
+}
+
+void StatsWrapper::appendAttribute(QQmlListProperty<AttributeWrapper> *list, AttributeWrapper *attribute)
+{
+    reinterpret_cast<StatsWrapper*>(list->data)->appendAttribute(attribute);
+}
+
+int StatsWrapper::attributeCount(QQmlListProperty<AttributeWrapper> *list)
+{
+    return reinterpret_cast<StatsWrapper*>(list->data)->attributeCount();
+}
+
+AttributeWrapper *StatsWrapper::attribute(QQmlListProperty<AttributeWrapper> *list, int index)
+{
+    return reinterpret_cast<StatsWrapper*>(list->data)->attribute(index);
+}
+
+void StatsWrapper::clearAttributes(QQmlListProperty<AttributeWrapper> *list)
+{
+    reinterpret_cast<StatsWrapper*>(list->data)->clearAttributes();
 }
--- CardManagerQml/cpp/Page/StatsWrapper.h
+++ CardManagerQml/cpp/Page/StatsWrapper.h
@@ -2,23 +2,43 @@
 #define STATSWRAPPER_H
 
 #include <QObject>
+#include <QList>
+#include <QQmlListProperty>
 
 class StatsData;
 class PersonalWrapper;
+class AttributeWrapper;
 
 class StatsWrapper : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(PersonalWrapper *personal READ personal CONSTANT)
+    Q_PROPERTY(PersonalWrapper *personal READ personal NOTIFY attributesChanged)
+    Q_PROPERTY(QQmlListProperty<AttributeWrapper> attributes READ attributes NOTIFY attributesChanged)
+
 public:
     explicit StatsWrapper(QObject *parent = nullptr);
     explicit StatsWrapper(StatsData *statsData, QObject *parent = nullptr);
 
     PersonalWrapper *personal() const;
+    QQmlListProperty<AttributeWrapper> attributes();
+    void appendAttribute(AttributeWrapper*);
+    int attributeCount() const;
+    AttributeWrapper *attribute(int) const;
+    void clearAttributes();
+
+signals:
+    void attributesChanged();
+
+private:
+    static void appendAttribute(QQmlListProperty<AttributeWrapper> *, AttributeWrapper *);
+    static int attributeCount(QQmlListProperty<AttributeWrapper>*);
+    static AttributeWrapper *attribute(QQmlListProperty<AttributeWrapper>*, int);
+    static void clearAttributes(QQmlListProperty<AttributeWrapper>*);
 
 private:
     StatsData *m_pStatsData{nullptr};
     PersonalWrapper *m_pPersonalWrapper{nullptr};
+    QList<AttributeWrapper*> m_attributes;
 };
 
 #endif // STATSWRAPPER_H
