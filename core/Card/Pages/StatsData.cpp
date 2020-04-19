010254f7986dd53527e33c849ee2022ab6547324
Betrezen88 <michal.s.wyszynski@gmail.com>
2020-04-19 21:37:57 +0200

    Added method to handle tricks.

--- core/Card/Pages/StatsData.cpp
+++ core/Card/Pages/StatsData.cpp
@@ -2,6 +2,7 @@
 #include "../Elements/Stats/PersonalData.h"
 #include "../Elements/Stats/AttributeData.h"
 #include "../Elements/Stats/OtherSkillData.h"
+#include "../Elements/Stats/TrickData.h"
 
 StatsData::StatsData()
 {
@@ -23,6 +24,11 @@ QList<OtherSkillData *> StatsData::otherSkills() const
     return m_otherSkills;
 }
 
+QList<TrickData *> StatsData::tricks() const
+{
+    return m_tricks;
+}
+
 void StatsData::setPersonal(PersonalData *personal)
 {
     m_personal = personal;
@@ -36,4 +42,9 @@ void StatsData::setAttributes(QList<AttributeData *> attributes)
 void StatsData::setOtherSkills(QList<OtherSkillData *> otherSkills)
 {
     m_otherSkills = otherSkills;
+}
+
+void StatsData::setTricks(QList<TrickData *> tricks)
+{
+    m_tricks = tricks;
 }
--- core/Card/Pages/StatsData.h
+++ core/Card/Pages/StatsData.h
@@ -9,6 +9,7 @@
 class PersonalData;
 class AttributeData;
 class OtherSkillData;
+class TrickData;
 
 class CORE_EXPORT StatsData
 {
@@ -18,15 +19,18 @@ public:
     PersonalData *personal() const;
     QList<AttributeData*> attributes() const;
     QList<OtherSkillData*> otherSkills() const;
+    QList<TrickData*> tricks() const;
 
     void setPersonal(PersonalData *personal);
     void setAttributes(QList<AttributeData*> attributes);
     void setOtherSkills(QList<OtherSkillData*> otherSkills);
+    void setTricks(QList<TrickData*> tricks);
 
 private:
     PersonalData *m_personal{nullptr};
     QList<AttributeData*> m_attributes;
     QList<OtherSkillData*> m_otherSkills;
+    QList<TrickData*> m_tricks;
 };
 
 #endif // STATSDATA_H
