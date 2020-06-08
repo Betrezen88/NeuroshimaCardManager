55f134c130e7d584ca5f358b6784ef1f82e430cb
Betrezen88 <michal.s.wyszynski@gmail.com>
2020-06-08 19:56:19 +0200

    Registered new classes to QML.

--- CardManagerQml/main.cpp
+++ CardManagerQml/main.cpp
@@ -8,10 +8,40 @@
 #include "cpp/Creators/Data/Origin.h"
 #include "cpp/Creators/Data/Feature.h"
 
+#include "Managers/MainManager.h"
+#include "Managers/CardManager.h"
+
+#include "Card/Data.h"
+#include "Card/Card.h"
+#include "Card/Pages/Stats.h"
+#include "Card/Elements/Stats/Attribute.h"
+#include "Card/Elements/Stats/Disease.h"
+#include "Card/Elements/Stats/OtherSkill.h"
+#include "Card/Elements/Stats/Penalty.h"
+#include "Card/Elements/Stats/Skillpack.h"
+#include "Card/Elements/Stats/Skill.h"
+#include "Card/Elements/Stats/Symptom.h"
+#include "Card/Elements/Stats/Trick.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    qmlRegisterType<MainManager>("core", 1, 0, "NSMainManager");
+    qmlRegisterType<CardManager>("core", 1, 0, "NSCardManager");
+    qmlRegisterType<Data>("core", 1, 0, "NSData");
+    qmlRegisterType<Card>("core", 1, 0, "NSCard");
+    qRegisterMetaType<Page::Type>("Page::Type");
+    qmlRegisterType<Page>("core", 1, 0, "NSPage");
+    qmlRegisterType<Stats>("core", 1, 0, "NSStats");
+    qmlRegisterType<Attribute>("core", 1, 0, "NSAttribute");
+    qmlRegisterType<Disease>("core", 1, 0, "NSDisease");
+    qmlRegisterType<OtherSkill>("core", 1, 0, "NSOtherSkill");
+    qmlRegisterType<Penalty>("core", 1, 0, "NSPenalty");
+    qmlRegisterType<Skillpack>("core", 1, 0, "NSSkillpack");
+    qmlRegisterType<Skill>("core", 1, 0, "NSSkill");
+    qmlRegisterType<Symptom>("core", 1, 0, "NSSymptom");
+    qmlRegisterType<Trick>("core", 1, 0, "NSTrick");
 
     qmlRegisterType<HeroCreatorData>("cpp", 1, 0, "HeroCreatorData");
     qmlRegisterType<Origin>("cpp", 1, 0, "Origin");
