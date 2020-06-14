b02b1d8348108d5ca592ee56708f0a5d19bb890c
Betrezen88 <michal.s.wyszynski@gmail.com>
2020-06-14 13:39:13 +0200

    Added class PageCreator.

---
+++ core/Creators/PageCreator.h
@@ -0,0 +1,35 @@
+﻿#ifndef PAGECREATOR_H
+#define PAGECREATOR_H
+
+#include <QObject>
+
+#include "core_global.h"
+
+class CORE_EXPORT PageCreator : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(Type type READ type CONSTANT)
+
+public:
+    enum class Type { STATS,
+                      NOTES,
+                      EQUIPMENT,
+                      BEAST,
+                      MACHINE,
+                      VEHICLE,
+                      FRIEND };
+    Q_ENUM(Type)
+
+    explicit PageCreator(QObject *parent = nullptr) : QObject(parent) {}
+    explicit PageCreator(const Type &type, QObject *parent = nullptr)
+        : QObject(parent),
+          m_type(type)
+    {}
+
+    Type type() const { return m_type; }
+
+private:
+    Type m_type;
+};
+
+#endif // PAGECREATOR_H
