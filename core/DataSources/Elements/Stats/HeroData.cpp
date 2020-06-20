563a3873e79cc44c6ab1e6d71f8217a76ecd3a09
Betrezen88 <michal.s.wyszynski@gmail.com>
2020-06-20 11:16:07 +0200

    Added logic of adding feature to HeroData, expose properties to qml.

--- core/DataSources/Elements/Stats/HeroData.cpp
+++ core/DataSources/Elements/Stats/HeroData.cpp
@@ -16,9 +16,15 @@ HeroData::HeroData(const QString &name,
 
 }
 
+QString HeroData::image() const
+{
+    return m_image;
+}
+
 void HeroData::addFeature(Feature *feature)
 {
     m_features.push_back(feature);
+    emit featuresChanged();
 }
 
 void HeroData::removeFeature(const QString &name)
@@ -27,6 +33,34 @@ void HeroData::removeFeature(const QString &name)
         if ( name == m_features.at(i)->name() ) {
             Feature *pFeatute = m_features.takeAt(i);
             delete pFeatute;
+            emit featuresChanged();
             break;
         }
+}
+
+QQmlListProperty<Feature> HeroData::features()
+{
+    return QQmlListProperty<Feature>(reinterpret_cast<Data*>(this), this,
+                                     &HeroData::featuresCount,
+                                     &HeroData::feature);
+}
+
+Feature *HeroData::feature(const int &index) const
+{
+    return m_features.at(index);
+}
+
+int HeroData::featuresCount() const
+{
+    return m_features.count();
+}
+
+Feature *HeroData::feature(QQmlListProperty<Feature> *list, int index)
+{
+    return reinterpret_cast<HeroData*>(list->data)->feature(index);
+}
+
+int HeroData::featuresCount(QQmlListProperty<Feature> *list)
+{
+    return reinterpret_cast<HeroData*>(list->data)->featuresCount();
 }
