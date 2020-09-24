50e6ccf71e6c975bb50efbabb3d9f25ec1d9b9cd
Betrezen88 <michal.s.wyszynski@gmail.com>
2020-09-24 23:15:17 +0200

    Added functionality of NSTrick to Trick.

--- core/Card/Elements/Stats/Trick.cpp
+++ core/Card/Elements/Stats/Trick.cpp
@@ -16,7 +16,51 @@ Trick::Trick(const QString &name,
 
 }
 
+Trick::Trick(const QString &name,
+             const QString &description,
+             const QString &action,
+             QVector<Requirement *> &requirements,
+             QObject *parent)
+    : Data(name, description, parent),
+      m_action(action),
+      m_requirements(requirements)
+{
+
+}
+
 QString Trick::action() const
 {
     return m_action;
+}
+
+QQmlListProperty<Requirement> Trick::requirements()
+{
+    return QQmlListProperty<Requirement>(reinterpret_cast<Data*>(this), this,
+                                         &Trick::requirementsCount,
+                                         &Trick::requirement);
+}
+
+int Trick::requirementsCount() const
+{
+    return m_requirements.count();
+}
+
+Requirement *Trick::requirement(const int &index) const
+{
+    return m_requirements.at(index);
+}
+
+bool Trick::hasRequirements() const
+{
+    return m_requirements.count() > 0;
+}
+
+int Trick::requirementsCount(QQmlListProperty<Requirement> *list)
+{
+    return reinterpret_cast<Trick*>(list->data)->requirementsCount();
+}
+
+Requirement *Trick::requirement(QQmlListProperty<Requirement> *list, int index)
+{
+    return reinterpret_cast<Trick*>(list->data)->requirement(index);
 }
--- core/Card/Elements/Stats/Trick.h
+++ core/Card/Elements/Stats/Trick.h
@@ -5,20 +5,42 @@
 
 #include "core_global.h"
 
+#include <QQmlListProperty>
+
+class Requirement;
+
 class CORE_EXPORT Trick : public Data
 {
     Q_OBJECT
+    Q_PROPERTY(QString action READ action CONSTANT)
+    Q_PROPERTY(QQmlListProperty<Requirement> requirements READ requirements CONSTANT)
 public:
-    Trick(QObject *parent = nullptr);
-    Trick(const QString &name,
-          const QString &description,
-          const QString &action,
-          QObject *parent = nullptr);
+    explicit Trick(QObject *parent = nullptr);
+    explicit Trick(const QString &name,
+                   const QString &description,
+                   const QString &action,
+                   QObject *parent = nullptr);
+    explicit Trick(const QString &name,
+                   const QString &description,
+                   const QString &action,
+                   QVector<Requirement*> &requirements,
+                   QObject *parent = nullptr);
 
     QString action() const;
 
+    QQmlListProperty<Requirement> requirements();
+    int requirementsCount() const;
+    Requirement* requirement(const int &index) const;
+
+    Q_INVOKABLE bool hasRequirements() const;
+
+private:
+    static int requirementsCount(QQmlListProperty<Requirement> *list);
+    static Requirement* requirement(QQmlListProperty<Requirement> *list, int index);
+
 private:
     QString m_action;
+    QVector<Requirement*> m_requirements;
 };
 
 #endif // TRICK_H
